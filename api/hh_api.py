2815f41a32abeb9e1140ea0cd83eebb3fab18cc8
Maxim Donchuk <m.donchuk@gmail.com>
2023-09-25 14:46:00 +0300

    Refactor  HeadHunterParser class

--- api/hh_api.py
+++ api/hh_api.py
@@ -3,17 +3,17 @@ from utils.safe_request import get_request_safe
 
 class HeadHunterParser:
     def __init__(self):
-        self.api_url = "https://api.hh.ru/"
+        self.__api_url = "https://api.hh.ru/"
         self.user_agent = "VacancyHelperApp/0.1"
-        self.host = "hh.ru"
+        self.__host = "hh.ru"
         # self.search_params = {}
 
     def _get_page_data(self, employer_id: str, page: int) -> dict:
         headers = {"HH-User-Agent": self.user_agent}
-        url = self.api_url + "vacancies"
+        url = self.__api_url + "vacancies"
         params = {
             "employer_id": employer_id,
-            "host": self.host,
+            "host": self.__host,
             "page": page,
             "per_page": 100,
             # "period": 7,
@@ -23,7 +23,7 @@ class HeadHunterParser:
         response = get_request_safe(url=url, headers=headers, params=params)
         return response.json()
 
-    def _get_raw_vacancies(self, employer_id: str) -> list[dict]:
+    def get_vacancies_by_employer(self, employer_id: str) -> list[dict]:
         first_page = self._get_page_data(employer_id, 0)
 
         items: list = first_page["items"]
@@ -31,15 +31,17 @@ class HeadHunterParser:
         total_pages = first_page["pages"]
 
         if total_pages > 1:
+
             for page in range(1, total_pages):
                 page_items = self._get_page_data(employer_id, page)["items"]
                 items.extend(page_items)
+
         return items
 
 
 if __name__ == "__main__":
     api = HeadHunterParser()
 
-    hh_vacancies = api._get_raw_vacancies("1455")
+    hh_vacancies = api.get_vacancies_by_employer("1455")
 
     print(len(hh_vacancies))
