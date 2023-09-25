83eaf66203d4b0be438e2745b5b2a036ba076e35
Maxim Donchuk <m.donchuk@gmail.com>
2023-09-25 17:10:34 +0300

    pytest installed

--- api/hh_api.py
+++ api/hh_api.py
@@ -4,12 +4,12 @@ from utils.safe_request import get_request_safe
 class HeadHunterParser:
     def __init__(self):
         self.__api_url = "https://api.hh.ru/"
-        self.user_agent = "VacancyHelperApp/0.1"
+        self.__user_agent = "VacancyHelperApp/0.1"
         self.__host = "hh.ru"
+        self.__headers = {"HH-User-Agent": self.__user_agent}
         # self.search_params = {}
 
-    def _get_page_data(self, employer_id: str, page: int) -> dict:
-        headers = {"HH-User-Agent": self.user_agent}
+    def _get_vacancies_page_data(self, employer_id: str, page: int) -> dict:
         url = self.__api_url + "vacancies"
         params = {
             "employer_id": employer_id,
@@ -20,11 +20,11 @@ class HeadHunterParser:
             # "schedule": "remote",
             "only_with_salary": False,
         }
-        response = get_request_safe(url=url, headers=headers, params=params)
+        response = get_request_safe(url=url, headers=self.__headers, params=params)
         return response.json()
 
     def get_vacancies_by_employer(self, employer_id: str) -> list[dict]:
-        first_page = self._get_page_data(employer_id, 0)
+        first_page = self._get_vacancies_page_data(employer_id, 0)
 
         items: list = first_page["items"]
 
@@ -33,15 +33,34 @@ class HeadHunterParser:
         if total_pages > 1:
 
             for page in range(1, total_pages):
-                page_items = self._get_page_data(employer_id, page)["items"]
+                page_items = self._get_vacancies_page_data(employer_id, page)["items"]
                 items.extend(page_items)
 
         return items
+
+    def get_employer_info(self, employer_id: str):
+        """Method takes information about employer and returns as a dict"""
+        request_url = f"{self.__api_url}employers/{employer_id}"
+
+        response = get_request_safe(url=request_url, headers=self.__headers)
+
+        if response.status_code == 404:
+            raise ValueError(f"Работодатель с id {employer_id} не найден")
+
+        return response.json()
+
+    def set_user_agent(self, new_user_agent: str) -> None:
+        """
+        Method changes HH-User-Agent parameter.
+        It may be useful when changing the name of the application.
+        """
+        self.__user_agent = new_user_agent
 
 
 if __name__ == "__main__":
     api = HeadHunterParser()
 
-    hh_vacancies = api.get_vacancies_by_employer("1455")
+    hh_vacancies = api.get_employer_info("1455")
 
     print(len(hh_vacancies))
+    print(hh_vacancies)
---
+++ models/models.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field, AliasPath
+from pydantic.functional_validators import field_validator
+import re
+
+
+class Employer(BaseModel):
+    id: int
+    name: str
+    open_vacancies: int
+    area: str = Field(validation_alias=AliasPath("area", "name"))
+    url: str = Field(validation_alias="alternate_url")
+    description: str
+
+    @field_validator("description")
+    def remove_html_tags(self, description_text: str):
+        """Removes html tags in description"""
+        return re.sub(re.compile("<.*?>"), "", description_text)
+
+
+class Vacancy(BaseModel):
+    id: int
+    name: str
+    employer_id: int = Field(validation_alias=AliasPath("employer", "id"))
+    area: str = Field(validation_alias=AliasPath("area", "name"))
+    salary_from: int | None = Field(validation_alias=AliasPath("salary", "from"))
+    salary_to: int | None = Field(validation_alias=AliasPath("salary", "to"))
+    salary_currency: str | None = Field(
+        validation_alias=AliasPath("salary", "currency")
+    )
+    url: str
+    requirement: str | None = Field(
+        validation_alias=AliasPath("snippet", "requirement")
+    )
+    responsibility: str | None = Field(
+        validation_alias=AliasPath("snippet", "responsibility")
+    )
